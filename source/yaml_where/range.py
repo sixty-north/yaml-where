40b9a181d39bf11c5da8f6752087d87a0564935c
Austin Bingham <austin@sixty-north.com>
2024-03-21 13:56:29 +0100

    Added Range.beginning() named constructor.

--- source/yaml_where/range.py
+++ source/yaml_where/range.py
@@ -15,3 +15,8 @@ class Range:
     "Half-open range describing a span inside a document."
     start: Position
     end: Position
+
+    @classmethod
+    def beginning(cls, length=1):
+        "Create a range that starts at the beginning of the document."
+        return cls(Position(0, 0), Position(0, length))
