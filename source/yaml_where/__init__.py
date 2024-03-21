71b3a1d6461db40d10dfa3e4c98ea75b8d039846
Austin Bingham <austin@sixty-north.com>
2024-03-21 13:56:14 +0100

    Exposed some exceptions from top-level package.

--- source/yaml_where/__init__.py
+++ source/yaml_where/__init__.py
@@ -1,8 +1,11 @@
 from .version import __version__, __version_info__
 
 from .yaml_where import YAMLWhere
+from .exceptions import MissingKeyError, UndefinedAccessError
 
 
 __all__ = [
     "YAMLWhere",
+    "MissingKeyError",
+    "UndefinedAccessError",
 ]
