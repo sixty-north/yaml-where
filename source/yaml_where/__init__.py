c63fbda1fc5529c66770ec1e567242dec0cdb760
Austin Bingham <austin.binghm@pm.me>
2025-02-24 07:58:14 +0100

    Hoisted a few names into top-level package.

--- source/yaml_where/__init__.py
+++ source/yaml_where/__init__.py
@@ -2,10 +2,15 @@ from .version import __version__, __version_info__
 
 from .yaml_where import YAMLWhere
 from .exceptions import MissingKeyError, UndefinedAccessError
+from .range import Range, Position
 
 
 __all__ = [
-    "YAMLWhere",
+    "__version__",
+    "__version_info__",
     "MissingKeyError",
+    "Position",
+    "Range",
     "UndefinedAccessError",
+    "YAMLWhere",
 ]
